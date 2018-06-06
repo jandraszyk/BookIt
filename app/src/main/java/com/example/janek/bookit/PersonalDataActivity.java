ea7d8f27ad3ac9980b843760fe3e0568b072173f
Dzialo3 <39697449+Dzialo3@users.noreply.github.com>
2018-06-06 20:08:34 +0200

    Create empty layout for personal data activity

--- app/src/main/java/com/example/janek/bookit/PersonalDataActivity.java
+++ app/src/main/java/com/example/janek/bookit/PersonalDataActivity.java
@@ -1,6 +1,13 @@
 package com.example.janek.bookit;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class PersonalDataActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data);
+
+    }
 }
